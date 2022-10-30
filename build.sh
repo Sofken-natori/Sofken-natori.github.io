899c2b7dcf58b4cf8dfade6d0d2028de43f5b76b
Sora Tonami <ms0503@outlook.com>
2022-10-31 00:12:24 +0900

    ビルドスクリプトの修正。

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 # shellcheck disable=SC2046
 # shellcheck disable=SC2086
 # shellcheck disable=SC2164
-BASE_DIR="$(cd $(basename "$0"); pwd)"
+BASE_DIR="$(cd $(dirname "$0"); pwd)"
 cd $BASE_DIR
-cp -r $BASE_DIR $BASE_DIR/out
-rm -rf $BASE_DIR/out/{.git{,hub},node_modules,template,build.sh,package.json,README.md,yarn.lock}
+cp -r $BASE_DIR $BASE_DIR/../out
+mv $BASE_DIR/../out $BASE_DIR/out
+rm -rf $BASE_DIR/out/{.git{,hub},.idea,README.md,build.sh,lib/common.scss,node_modules,package.json,template,yarn.lock}
 yarn exec sass lib/:out/lib/ --no-source-map
 exit 0
