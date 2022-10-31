8f4d292a4f786be7ee2b56a18fe5bed1f54482be
Sora Tonami <ms0503@outlook.com>
2022-11-01 00:13:05 +0900

    最小化ツールを導入。

--- build.sh
+++ build.sh
@@ -7,10 +7,14 @@ echo 'cd $(cd $(dirname "$0"); pwd)';cd $(cd $(dirname "$0"); pwd)
 echo 'Copy resource files'
 echo 'cp -r .{,./out}';cp -r .{,./out}
 echo 'mv .{.,}/out';mv .{.,}/out
-echo 'Compile SCSS'
+echo 'Compile and Minify SCSS'
 echo 'rm out/lib/common.css';rm out/lib/common.css
 echo 'yarn exec -- sass lib/common.scss:out/lib/common-1.css --no-source-map';yarn exec -- sass lib/common.scss:out/lib/common-1.css --no-source-map
-echo 'yarn exec -- postcss out/lib/common-1.css -o out/lib/common.css --no-map';yarn exec -- postcss out/lib/common-1.css -o out/lib/common.css --no-map
+echo 'yarn exec -- postcss out/lib/common-1.css -o out/lib/common-2.css --no-map';yarn exec -- postcss out/lib/common-1.css -o out/lib/common-2.css --no-map
+echo 'yarn exec -- cleancss -o out/lib/common.css out/lib/common-2.css';yarn exec -- cleancss -o out/lib/common.css out/lib/common-2.css
+echo 'Minify JavaScript'
+echo 'rm out/lib/slider.js';rm out/lib/slider.js
+echo 'yarn exec -- uglifyjs lib/slider.js -c -o out/lib/slider.js';yarn exec -- uglifyjs lib/slider.js -c -o out/lib/slider.js
 echo 'Expand Components'
 if [[ "$1" = "unofficial" ]]; then
     echo 'sed -i '\''s/{{ROOT}}/\/Sofken-natori.github.io/g'\'' components/navigation.html';sed -i 's/{{ROOT}}/\/Sofken-natori.github.io/g' components/navigation.html
@@ -21,5 +25,5 @@ echo 'find out -name '\''*.html'\'' | xargs -i sed -i -e '\''/{{HEADER}}/r compo
 echo 'find out -name '\''*.html'\'' | xargs -i sed -i -e '\''/{{NAVIGATION}}/r components/navigation.html'\'' {} -e '\''/{{NAVIGATION}}/d'\';find out -name '*.html' | xargs -i sed -i -e '/{{NAVIGATION}}/r components/navigation.html' {} -e '/{{NAVIGATION}}/d'
 echo 'find out -name '\''*.html'\'' | xargs -i sed -i -e '\''/{{FOOTER}}/r components/footer.html'\'' {} -e '\''/{{FOOTER}}/d'\';find out -name '*.html' | xargs -i sed -i -e '/{{FOOTER}}/r components/footer.html' {} -e '/{{FOOTER}}/d'
 echo 'Delete unnecessary files'
-echo 'rm -rf out/{.git{,hub},.idea,README.md,build.sh,components,lib/common-1.css{,.map},lib/common.scss,node_modules,package.json,template,yarn.lock}';rm -rf out/{.git{,hub},.idea,README.md,build.sh,components,lib/common-1.css{,.map},lib/common.scss,node_modules,package.json,template,yarn.lock}
+echo 'rm -rf out/{.git{,hub},.idea,README.md,build.sh,components,lib/common-{1,2}.css{,.map},lib/common.scss,node_modules,package.json,template,yarn.lock}';rm -rf out/{.git{,hub},.idea,README.md,build.sh,components,lib/common-{1,2}.css{,.map},lib/common.scss,node_modules,package.json,template,yarn.lock}
 exit 0
--- postcss.config.mjs
+++
@@ -1,5 +0,0 @@
-export const module = {
-    plugins: [
-        import('autoprefixer')
-    ]
-};
