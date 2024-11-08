00b5791cbef86bdcfa463784cfdfadf16824a266
Sora Tonami <ms0503@outlook.com>
2024-11-08 15:25:24 +0900

    PDF.jsのワーカーの参照先を変更。

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import App from './app.tsx';
 import './index.scss';
-import { dependencies } from '@/package.json';
 import { Worker } from '@react-pdf-viewer/core';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import pdfWorker from 'pdfjs-dist/build/pdf.worker.min';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${dependencies['pdfjs-dist']}/build/pdf.worker.min.js`}>
+        <Worker workerUrl={pdfWorker}>
             <BrowserRouter>
                 <App />
             </BrowserRouter>
